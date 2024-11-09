cb6d8fde34c8f62d1e83fae948d327716607075e
Brendan Smiley <bsmiley.lego@gmail.com>
2024-11-09 13:32:40 -0700

    COMPLETE: Navigation fronted

---
+++ frontend/src/pages/live-view/LiveViewPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+
+function LiveViewPage() {
+  return (
+    <div>
+      <NavBar />
+      LiveViewPage
+    </div>
+  );
+}
+export default LiveViewPage;
