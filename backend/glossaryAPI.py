6b71a38527c2b9e27e6a3cf7dd1140b0ca8b4fd2
Cody Casselman <cody.casselman@ucalgary.ca>
2024-11-09 15:35:05 -0700

    Update glossaryAPI.py
    updated glossaryAPI to provide internal api endpoints to send gpt data

--- backend/glossaryAPI.py
+++ backend/glossaryAPI.py
@@ -1,21 +1,71 @@
 import openai
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, app
 from secure import OPENAI_API_KEY
 from gpt_interaction import get_legal_explanation_and_usage
+import os
+import json
+import time
+
 
 CACHE_FILE = './data/termsUsageExplanations.json'
 
 
-def get_cached_data(key):
-    # Check if key is in the cache and if it hasn't expired
-    '''if key in cache:
-        cached_entry = cache[key]
-        if time.time() - cached_entry['timestamp'] < CACHE_TIMEOUT:
-            return cached_entry['data']'''
-    
+def load_cache():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, 'r') as file:
+            return json.load(file)
+    return {}
+
+def save_cache(cache_data):
+    with open(CACHE_FILE, 'w') as file:
+        json.dump(cache_data, file)
+
+
+
+def get_cached_data(term):
+    '''
+    Checks if a term is in the cache and returns its JSON data if it is
+
+    args:
+        term (str): the legal term to be checked
+
+    returns:
+        JSON: the JSON data assosciated with the gpt entries of the term
+        None: the term was not in the cache
+    '''
+    cache = load_cache()
+    if term in cache:
+        cached_entry = cache[term]
+        return cached_entry[term]
     return None
 
 
-@app.route('backend/get_explanation', methods=['POST'])
-def get_explanation(keyword):
 
+@app.route('/api/get_gpt_response', methods=['POST'])
+def get_gpt_response(term):
+    try:
+        termJSON = get_cached_data(term)
+        if termJSON:
+            #term in cache
+            return jsonify({"message": termJSON}), 200
+        #term not in cache
+        #generate gpt response
+        new_entry = json.dumps(get_legal_explanation_and_usage(term))
+        #add to cache
+        cache = load_cache()
+        cache[term] = new_entry
+        #save updated cache
+        save_cache(cache)
+        return jsonify({"message": new_entry}), 200
+    except:
+        return jsonify({"message": "Couldn't open file"}), 400
+
+
+@app.route('/api/clear_cache', methods=['POST'])
+def clear_cache():
+    try:
+        with open(CACHE_FILE, 'w') as file:
+            json.dump({}, file) #write an empty file to the cache location
+        return jsonify({"message": "Cache cleared"}), 200
+    except:
+        return jsonify({"message": "Couldn't open file"}), 400
